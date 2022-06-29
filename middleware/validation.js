11d2e56e2b9cd461a786d38e4277c7599032e8c0
Gopikrishna13 <108319104+Gopikrishna13@users.noreply.github.com>
2022-06-29 11:43:06 +0530

    Update validation.js

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,1 +1,25 @@
+const Joi = require('joi');
 
+const validation = Joi.object({
+
+    name: Joi.string().min(5).max(30).regex(/^[a-zA-Z]+$/).required(),
+
+    mail: Joi.string()
+      .email()
+      .min(5)
+      .max(50)
+      .required(),
+    address: {
+      street: Joi.string().min(5).max(30),
+      city: Joi.string().min(5).max(30),
+      country: Joi.string().min(5).max(30)
+    },
+    socialmedia: {
+      facebook: Joi.string().min(5).max(30),
+      twitter: Joi.string().min(5).max(30)
+    }
+
+  });
+
+
+  module.exports=validation;
